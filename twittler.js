05a44015190d72919c15b1c54eb56f3096c3849d
Paige <paige.vogenthaler@gmail.com>
2016-10-25 16:27:10 -0700

    Updated: viewing specific user's tweets

--- twittler.js
+++ twittler.js
@@ -16,32 +16,28 @@ $(document).ready(function(){
       $tweet.appendTo($section);
       index -= 1;
     }
-  });
 
+    $(".user").on('click', function() {
+      //remove all tweets appended to section
+      $('div.tweet').remove();
 
-  //Allow the user to click on a username to see that user's timeline 
-  $(".user").on('click', function(){    
-    var $section = $('section');
-    $section.html('');
+      //get username
+      var user = $(this).text();
+      user = user.slice(1, -1);
 
-    //remove all tweets appended to section
-    $('div.tweet').remove();
-
-    //defines username
-    var user = $(this).text();
-    user = user.slice(1, -1);
-
-    //iterate through user's tweets
-    var index = streams.users.user.length - 1;
-    while(index >= 0){
-      var tweet = streams.users.user[index];
-      var $tweet = $('<div class="tweet"></div>');
-      //format tweet
-      $tweet.text("@" + user +": " + tweet.message + " (" + tweet.created_at + ")");
-      //append tweet to section
-      $tweet.appendTo($section);
-      index -= 1;
-    }
+      //iterate through user's tweets
+      console.log(streams.users);
+      var index = streams.users[user].length - 1;
+      while(index >= 0){
+        var tweet = streams.users[user][index];
+        var $tweet = $('<div class="tweet"></div>');
+        //format tweet
+        $tweet.text("@" + user +": " + tweet.message + " (" + tweet.created_at + ")");
+        //append tweet to section
+        $tweet.appendTo($section);
+        index -= 1;
+      }
+    });
   });
   
 });
